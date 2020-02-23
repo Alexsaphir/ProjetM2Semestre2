e0073012e5327737ba03d08e5246ce266d0a59ac
Alexandre <git@alexsaphir.com>
2020-02-23 18:09:35 +0100

    Script is able to read conf from the fortran namelist file

--- Calcul Parallele/Projet1-MPI/script/post-traitement.py
+++ Calcul Parallele/Projet1-MPI/script/post-traitement.py
@@ -1,2 +1,41 @@
 import f90nml
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+
+def main():
+    print('Post processing :')
+    # Lecture du fichier conf.nml
+    nml: f90nml.namelist = f90nml.read('conf.nml')
+
+    # Chargement des paramètres
+    L: float = nml['conf']['L']
+    B: float = nml['conf']['B']
+    alpha: float = nml['conf']['alpha']
+    Ns: int = nml['conf']['Ns']
+    Nk: int = nml['conf']['Nk']
+    Nx: int = nml['conf']['Nx']
+    Ny: int = nml['conf']['Ny']
+    filename = nml['conf']['filename']
+
+    x = np.linspace(0., L, Nx)
+    y = np.linspace(0., B, Ny)
+
+    data = np.genfromtxt(filename)
+
+    hf = plt.figure()
+    ha = hf.add_subplot(111, projection='3d')
+
+    X, Y = np.meshgrid(x, y)  # `plot_surface` expects `x` and `y` data to be 2D
+    ha.plot_surface(X, Y, data)
+
+    plt.show()
+
+    plt.show()
+
+    print(X)
+
+
+if __name__ == '__main__':
+    main()
