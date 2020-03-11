00d093986ecf53ee294ee4ed0a4cec0e9144acee
Shild <kearhoor@hotmail.fr>
2020-03-11 10:56:45 +0100

    Auto stash before merge of "master" and "origin/master"

--- Plasma/Projet/src/gridfd.h
+++ Plasma/Projet/src/gridfd.h
@@ -13,8 +13,8 @@ public:
 
 	[[nodiscard]] double getL() const;
 	[[nodiscard]] double getVmax() const;
-	[[nodiscard]] uint	 getNx() const;
-	[[nodiscard]] uint	 getNv() const;
+	[[nodiscard]] uint getNx() const;
+	[[nodiscard]] uint getNv() const;
 
 	//    Pour un i donné retourne la valeur de x dans l'intervalle [0,L]
 	[[nodiscard]] double getX(int i) const;
@@ -23,28 +23,27 @@ public:
 
 	void init_f(double f0(double));
 
-	[[nodiscard]] double  E(int p, int v) const;
-	[[nodiscard]] double& E(int p, int v);
+	[[nodiscard]] double E(int p) const;
+	[[nodiscard]] double &E(int p);
 
-	[[nodiscard]] double  f(int p, int v) const;
-	[[nodiscard]] double& f(int p, int v);
+	[[nodiscard]] double f(int p, int v) const;
+	[[nodiscard]] double &f(int p, int v);
 
-	[[nodiscard]] double  Rho(int p) const;
-	[[nodiscard]] double& Rho(int p);
+	[[nodiscard]] double Rho(int p) const;
+	[[nodiscard]] double &Rho(int p);
 
 	void print() const;
 
-
 private:
-	double m_L {1.};
-	double m_Vmax {10.};
+	double m_L{1.};
+	double m_Vmax{10.};
 
-	uint m_Nx {10};
-	uint m_Nv {10};
+	uint m_Nx{10};
+	uint m_Nv{10};
 
 	std::vector<std::vector<double>> m_f; // distribution of the plasma
-	std::vector<double>				 m_E; // Electric field
-	std::vector<double>				 m_rho; // Electric charge
+	std::vector<double> m_E;			  // Electric field
+	std::vector<double> m_rho;			  // Electric charge
 };
 
 #endif // GRIDFD_H
--- Plasma/Projet/src/main.cpp
+++ Plasma/Projet/src/main.cpp
@@ -4,10 +4,11 @@
 
 #include "gridfd.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    GridFD  G(1,1,3,3);
-    G.print();
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+	GridFD G(1, 1, 3, 3);
+	G.print();
 
-    return 0;
+	return 0;
 }
