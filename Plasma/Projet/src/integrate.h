a39924b99f17932cd122abb0996a7cbba72d5535
Alexandre <github@alexsaphir.com>
2020-03-11 10:05:46 +0100

    indentation

--- Plasma/Projet/src/integrate.h
+++ Plasma/Projet/src/integrate.h
@@ -3,15 +3,13 @@
 
 typedef unsigned int uint;
 
-template <typename in = double, typename out = double>
-class Integrate
+template <typename in = double, typename out = double> class Integrate
 {
-	double operator()(out f(in), in a, in b, uint N=128)
+	double operator()(out f(in), in a, in b, uint N = 128)
 	{
 		for (uint i = 0; i < N; ++i)
 		{
-			// TODO Write the code to perform the
-			//  integration
+			// TODO Write the code to perform the integration
 		}
 	}
 };
