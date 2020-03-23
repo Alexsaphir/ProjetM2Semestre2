72dbbd21a4eb185142ea74971d64dbccdec36cb0
Alexandre <Alexsaphir@users.noreply.github.com>
2020-03-23 02:42:03 +0100

    mise en forme

--- MilieuxPoreux/TPMACS-VF4-short-serveur/fsourcemod.f90
+++ MilieuxPoreux/TPMACS-VF4-short-serveur/fsourcemod.f90
@@ -2,120 +2,118 @@ Module fsourcemod
 
 Contains
 
- !======================================================
-!=====================================================
-  FUNCTION gbord(x,y,m)
-    ! Calcul la solution exacte de l'equation
-    !--------
-    ! Modules
-    !--------
-    USE longr
+    !======================================================
+    !=====================================================
+    FUNCTION gbord(x, y, m)
+        ! Calcul la solution exacte de l'equation
+        !--------
+        ! Modules
+        !--------
+        USE longr
 
-    IMPLICIT NONE
+        IMPLICIT NONE
 
-    !--------------------------
-    ! Declaration des arguments
-    !--------------------------
-    REAL(kind=long), INTENT(in)     :: x, y
-    integer :: m
-    REAL(kind=long)                 :: gbord
-    !----------------------------------
-    ! Declaration des variables locales
-    !----------------------------------
-    CHARACTER(len=6)      :: oldprf
-    REAL(kind=long)                 :: pi
+        !--------------------------
+        ! Declaration des arguments
+        !--------------------------
+        REAL(kind = long), INTENT(in) :: x, y
+        integer :: m
+        REAL(kind = long) :: gbord
+        !----------------------------------
+        ! Declaration des variables locales
+        !----------------------------------
+        CHARACTER(len = 6) :: oldprf
+        REAL(kind = long) :: pi
 
-    !-------------------
-    ! Debut du programme
-    !-------------------
-    oldprf = prefix
-    prefix = 'Gbord'
+        !-------------------
+        ! Debut du programme
+        !-------------------
+        oldprf = prefix
+        prefix = 'Gbord'
 
-    !------
-    ! Corps
-    !------
-    pi = 4.D0*ATAN(1.D0)
+        !------
+        ! Corps
+        !------
+        pi = 4.D0 * ATAN(1.D0)
 
-    select case(m)
-    case(1)
-       gbord = 1.
-    case(2)
-       gbord  = x+y
-    case(3) 
-       gbord  = x*x + y*y
-    case(4)
-       gbord = COS(5.*pi*(x+y))
-    case(5)
-       gbord = x*(1.-x)*y*(1.-y)
-    case(6)
-       gbord= SIN(pi*x)*SIN(pi*y)
-    case default 
-       print*, ' pb gbord'
-       stop
-    end select
-    !-----------------
-    ! Fin du programme
-    !-----------------
-    prefix = oldprf
+        select case(m)
+        case(1)
+            gbord = 1.
+        case(2)
+            gbord = x + y
+        case(3)
+            gbord = x * x + y * y
+        case(4)
+            gbord = COS(5. * pi * (x + y))
+        case(5)
+            gbord = x * (1. - x) * y * (1. - y)
+        case(6)
+            gbord = SIN(pi * x) * SIN(pi * y)
+        case default
+            print*, ' pb gbord'
+            stop
+        end select
+        !-----------------
+        ! Fin du programme
+        !-----------------
+        prefix = oldprf
 
-    RETURN
-  END FUNCTION gbord
-
-
+        RETURN
+    END FUNCTION gbord
 
 
-FUNCTION fsource(x,y, m)
-    !--------
-    ! Modules
-    !--------
-    USE longr
+    FUNCTION fsource(x, y, m)
+        !--------
+        ! Modules
+        !--------
+        USE longr
 
-    IMPLICIT NONE
+        IMPLICIT NONE
 
-    !--------------------------
-    ! Declaration des arguments
-    !--------------------------
-    REAL(kind=long), INTENT(in)     :: x , y
-    Integer :: m
-    REAL(kind=long)                 :: fsource
-    !----------------------------------
-    ! Declaration des variables locales
-    !----------------------------------
-    CHARACTER(len=6)                :: oldprf
-    REAL(kind=long)                 :: pi
-    REAL(kind=long) ::lea,leaa
-    !-------------------
-    ! Debut du programme
-    !-------------------
-    oldprf = prefix
-    prefix = 'FSOUR'
+        !--------------------------
+        ! Declaration des arguments
+        !--------------------------
+        REAL(kind = long), INTENT(in) :: x, y
+        Integer :: m
+        REAL(kind = long) :: fsource
+        !----------------------------------
+        ! Declaration des variables locales
+        !----------------------------------
+        CHARACTER(len = 6) :: oldprf
+        REAL(kind = long) :: pi
+        REAL(kind = long) :: lea, leaa
+        !-------------------
+        ! Debut du programme
+        !-------------------
+        oldprf = prefix
+        prefix = 'FSOUR'
 
 
-    !------
-    ! Corps
-    !------
-    pi=4.D0*ATAN(1.D0)
-    Select case (m)
-    case(1)
-       fsource =   theta
-    case (2)
-       fsource  = theta *(x+y)
-    case (3)
-       fsource = 
-    case(4)
-       fsource =  
-    case(5)
-       fsource = 
-    case(6)
-       fsource = 
-    end Select
-    !----------------
-    ! Fin du programme
-    !-----------------
-    prefix = oldprf
+        !------
+        ! Corps
+        !------
+        pi = 4.D0 * ATAN(1.D0)
+        Select case (m)
+        case(1)
+            fsource = theta
+        case (2)
+            fsource = theta * (x + y)
+        case (3)
+            fsource =
+        case(4)
+            fsource =
+        case(5)
+            fsource =
+        case(6)
+            fsource =
+        end Select
+        !----------------
+        ! Fin du programme
+        !-----------------
+        prefix = oldprf
 
-    RETURN
-  END FUNCTION fsource
+        RETURN
+    END FUNCTION fsource
 
 
 END Module fsourcemod
